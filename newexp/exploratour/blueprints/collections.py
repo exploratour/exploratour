88df1345ec354a7fe3994a9734f619f4dad76a9f
Richard Boulton <richard@tartarus.org>
2019-05-18 09:16:19 +0200

    Add browsing of collection children and parents

--- newexp/exploratour/blueprints/collections.py
+++ newexp/exploratour/blueprints/collections.py
@@ -45,6 +45,7 @@ tabs = []
 
 class CollTabData:
     ARGS = (
+        ("id", "", str),
         ("showfull", 0, int),
         ("startrank", 0, int),
         ("order", "collection", str),
@@ -52,18 +53,7 @@ class CollTabData:
     )
 
     def __init__(self):
-        self.collid = request.args.get("id")
-
-        try:
-            self.collection = Collection.query.filter(
-                Collection.id == self.collid
-            ).one()
-        except NoResultFound:
-            abort(404)
-
-        self.active_tabs = tuple(tab for tab in tabs if tab.active(self.collection))
-
-        self.args = {"id": self.collid}
+        self.args = {}
         for arg, default, cast in CollTabData.ARGS:
             try:
                 self.args[arg] = cast(request.args.get(arg, default))
@@ -71,18 +61,48 @@ class CollTabData:
             except ValueError:
                 abort(400, "Invalid value for argument {}", arg)
 
+        try:
+            self.collection = Collection.query.filter(Collection.id == self.id).one()
+        except NoResultFound:
+            abort(404)
+
+        self.active_tabs = tuple(tab for tab in tabs if tab.active(self.collection))
+
+    def link_args(self, **extra):
+        """Get the arguments needed for a link within the collection"""
+        non_default_args = {"id": self.id}
+        args = {}
+        args.update(self.args, **extra)
+        for arg, default, cast in CollTabData.ARGS:
+            if args[arg] != cast(default):
+                non_default_args[arg] = self.args[arg]
+        return non_default_args
+
     def template_params(self, **extra):
         result = {}
         result.update(
             dict(
-                id=self.collid,
+                id=self.id,
                 collection=self.collection,
                 active_tabs=self.active_tabs,
                 args=self.args,
+                link_args=self.link_args,
             ),
             **extra
         )
         return result
+
+
+@bp.route("/coll/children", methods=("GET",))
+def collection_children():
+    data = CollTabData()
+
+    return render_template(
+        "collections/children.html", **data.template_params(collections=data.collection.children)
+    )
+
+
+tabs.append(Tab("collection_children", "Children", lambda collection: collection.children))
 
 
 @bp.route("/coll/records", methods=("GET",))
@@ -108,3 +128,17 @@ def collection_records():
 
 
 tabs.append(Tab("collection_records", "Records", lambda _: True))
+
+
+@bp.route("/coll/parents", methods=("GET",))
+def collection_parents():
+    data = CollTabData()
+
+    return render_template(
+        "collections/parents.html", **data.template_params(collections=data.collection.parents)
+    )
+
+
+tabs.append(Tab("collection_parents", "Parent Collections", lambda collection: collection.parents))
+
+
