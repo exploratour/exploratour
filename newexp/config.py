807b3ee069663884cf3e2c2104ea1e131a1b0aec
Richard Boulton <boulton.rj@gmail.com>
2020-06-07 00:59:09 +0100

    Add media path

--- newexp/config.py
+++ newexp/config.py
@@ -2,4 +2,5 @@ import os
 
 TOP_DIR = os.path.dirname(os.path.realpath(os.path.abspath(__file__)))
 DATA_PATH = os.path.join(TOP_DIR, "data")
+MEDIA_PATH = os.path.join(TOP_DIR, "../media")
 DB_PATH = os.path.join(DATA_PATH, "db.sqlite")
